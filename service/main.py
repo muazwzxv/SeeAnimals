90f7709fdb13cf7a88764af7d5a016557a688fa3
muazwzxv <muazwazir@gmail.com>
2021-12-25 02:39:33 +0800

    rest enedpoint

--- service/main.py
+++ service/main.py
@@ -1,7 +1,10 @@
 import io
+import uvicorn
 from fastapi import FastAPI
 from PIL import Image
 from fastapi.datastructures import UploadFile, File
+from yolov5 import *
+from starlette.responses import Response
 
 app = FastAPI(
     title="Serving Yolov5",
@@ -15,7 +18,20 @@ async def home():
     return {"message": "Hello world"}
 
 
+# Endpoint to test using REST
 @app.post("/yolo")
 def process_yolo(file: UploadFile = File(...)):
     file_bytes = file.file.read()
     image = Image.open(io.BytesIO(file_bytes))
+
+    name = f"/data/{str(uuid.uuid4())}.png"
+
+    image.filename = name
+    classes, converted = yolov5(image)
+
+    bytes_io = io.BytesIO()
+
+    converted.save(name)
+    converted.save(bytes_io, format="png")
+
+    return Response(bytes_io.getvalue(), media_type="image/png")
